80ef57116e315b4f6e842879b07c344666eedc6a
huniversal <hunivers0523@naver.com>
2026-05-15 16:29:29 +0900

    feat: 전역 예외 처리 세팅

--- src/main/java/com/keeply/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/keeply/common/exception/GlobalExceptionHandler.java
@@ -1,1 +1,22 @@
+package com.keeply.common.exception;
 
+import com.keeply.common.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+  @ExceptionHandler(CustomException.class)
+  public ResponseEntity<ApiResponse<Void>> handleCustomException(CustomException e) {
+    ErrorCode errorCode = e.getErrorCode();
+    return ResponseEntity.status(errorCode.getHttpStatus())
+        .body(ApiResponse.failure(errorCode.getMessage()));
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ApiResponse<Void>> handleGeneralException(Exception e) {
+    return ResponseEntity.status(ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus())
+        .body(ApiResponse.failure(ErrorCode.INTERNAL_SERVER_ERROR.getMessage()));
+  }
+}
