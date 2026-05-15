5518c86367d8cb8d4dee8c1875624d9821cdf8a5
huniversal <hunivers0523@naver.com>
2026-05-15 14:09:20 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/keeply/KeeplyServerApplication.java
@@ -0,0 +1,13 @@
+package com.keeply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeeplyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeeplyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keeply/KeeplyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keeply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeeplyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
