cb4a96a7a33d2cd409911304036ab6ff22e221f6
huniversal <hunivers0523@naver.com>
2026-05-19 02:39:13 +0900

    feat: Enum 세팅

---
+++ src/main/java/com/keeply/common/entity/GroupRole.java
@@ -0,0 +1,6 @@
+package com.keeply.common.entity;
+
+public enum GroupRole {
+  OWNER,
+  STAFF
+}
---
+++ src/main/java/com/keeply/common/entity/NoticeTag.java
@@ -0,0 +1,6 @@
+package com.keeply.common.entity;
+
+public enum NoticeTag {
+  DAILY,
+  WEEKLY,
+}
