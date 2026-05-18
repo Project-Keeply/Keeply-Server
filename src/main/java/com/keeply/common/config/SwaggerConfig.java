3127d7f590d1a24c2e132dad96b6a9121f42d625
huniversal <hunivers0523@naver.com>
2026-05-19 01:43:29 +0900

    init: swaggerCongig 생성

---
+++ src/main/java/com/keeply/common/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.keeply.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI openAPI() {
+    return new OpenAPI()
+        .info(new Info().title("Keeply API").description("Keeply 서비스 API 문서").version("v1.0"));
+  }
+}
