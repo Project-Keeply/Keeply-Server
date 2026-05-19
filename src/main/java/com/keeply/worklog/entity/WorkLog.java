640d29ba16af081c3cf1168404d857473218db59
huniversal <hunivers0523@naver.com>
2026-05-19 14:12:26 +0900

    feat: Worklog Entity 생성

---
+++ src/main/java/com/keeply/worklog/entity/WorkLog.java
@@ -0,0 +1,47 @@
+package com.keeply.worklog.entity;
+
+import com.keeply.common.entity.BaseTimeEntity;
+import com.keeply.group.entity.Group;
+import com.keeply.group.entity.GroupMember;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinColumns;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "work_logs")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class WorkLog extends BaseTimeEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumns({
+    @JoinColumn(name = "group_id", referencedColumnName = "group_id"),
+    @JoinColumn(name = "author_user_id", referencedColumnName = "user_id")
+  })
+  private GroupMember authorMember;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "group_id", insertable = false, updatable = false)
+  private Group group;
+
+  @Column(nullable = false, columnDefinition = "TEXT")
+  private String content;
+}
