5fe1a2b493b0da3df3cbfdeae4c7b9e4c70a4927
huniversal <hunivers0523@naver.com>
2026-05-19 14:16:16 +0900

    feat: Expiry Entity 생성

---
+++ src/main/java/com/keeply/expiry/entity/ExpiryItem.java
@@ -0,0 +1,53 @@
+package com.keeply.expiry.entity;
+
+import com.keeply.common.entity.BaseTimeEntity;
+import com.keeply.group.entity.Group;
+import com.keeply.group.entity.GroupMember;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinColumns;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "expiry_items")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class ExpiryItem extends BaseTimeEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumns({
+    @JoinColumn(name = "group_id", referencedColumnName = "group_id"),
+    @JoinColumn(name = "author_user_id", referencedColumnName = "user_id")
+  })
+  private GroupMember authorMember;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "group_id", insertable = false, updatable = false)
+  private Group group;
+
+  @Column(name = "product_name", nullable = false)
+  private String productName;
+
+  @Column(name = "expire_date", nullable = false)
+  private LocalDate expireDate;
+
+  @Column(name = "image_url", nullable = false)
+  private String imageUrl;
+}
