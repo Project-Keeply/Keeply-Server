bee03e043836b083b0f1888780205f252ec08dac
huniversal <hunivers0523@naver.com>
2026-05-22 17:45:16 +0900

    feat: userRepository 구현

--- src/main/java/com/keeply/user/repository/UserRepository.java
+++ src/main/java/com/keeply/user/repository/UserRepository.java
@@ -1,3 +1,9 @@
 package com.keeply.user.repository;
 
-public class UserRepository {}
+import com.keeply.user.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByKakaoId(String kakaoId);
+}
