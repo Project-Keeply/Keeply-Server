fc4de3e016548ec6c60cb5164990a8577c95c3ba
huniversal <hunivers0523@naver.com>
2026-05-22 17:47:25 +0900

    feat: RefreshToken Repository 구현

--- src/main/java/com/keeply/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/keeply/auth/repository/RefreshTokenRepository.java
@@ -1,3 +1,12 @@
 package com.keeply.auth.repository;
 
-public class RefreshTokenRepository {}
+import com.keeply.auth.entity.RefreshToken;
+import com.keeply.user.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+  Optional<RefreshToken> findByUser(User user);
+
+  void deleteByUser(User user);
+}
