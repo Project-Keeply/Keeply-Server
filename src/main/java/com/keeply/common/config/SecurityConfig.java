f23c1f044cd40035afb6ed47c726a985338b7393
huniversal <hunivers0523@naver.com>
2026-05-23 16:20:45 +0900

    feat: Security 설정

--- src/main/java/com/keeply/common/config/SecurityConfig.java
+++ src/main/java/com/keeply/common/config/SecurityConfig.java
@@ -1,26 +1,35 @@
 package com.keeply.common.config;
 
+import com.keeply.auth.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-  private static final String[] SWAGGER_WHITELIST = {
-    "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html"
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+  private static final String[] WHITELIST = {
+    "/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html"
   };
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.csrf(AbstractHttpConfigurer::disable)
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(
-            auth ->
-                auth.requestMatchers(SWAGGER_WHITELIST).permitAll().anyRequest().authenticated());
+            auth -> auth.requestMatchers(WHITELIST).permitAll().anyRequest().authenticated())
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
   }
