7618bb09ce10aa3d8dcd212e542e2a9c329c58cb
huniversal <hunivers0523@naver.com>
2026-05-24 15:13:02 +0900

    feat: KakaoProperties 검증 어노테이션 추가

--- src/main/java/com/keeply/auth/kakao/KakaoProperties.java
+++ src/main/java/com/keeply/auth/kakao/KakaoProperties.java
@@ -1,16 +1,22 @@
 package com.keeply.auth.kakao;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
 @Component
+@Validated
 @ConfigurationProperties(prefix = "kakao")
 public class KakaoProperties {
-  private String clientId;
-  private String clientSecret;
-  private String redirectUri;
+
+  @NotBlank private String clientId;
+
+  @NotBlank private String clientSecret;
+
+  @NotBlank private String redirectUri;
 }
