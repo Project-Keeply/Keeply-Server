9bd58fcb83dc104833230fc138704fddf25338f4
huniversal <hunivers0523@naver.com>
2026-06-02 02:02:04 +0900

    feat: GroupMemberRespository 설계

--- src/main/java/com/keeply/group/repository/GroupMemberRepository.java
+++ src/main/java/com/keeply/group/repository/GroupMemberRepository.java
@@ -1,3 +1,18 @@
 package com.keeply.group.repository;
 
-public class GroupMemberRepository {}
+import com.keeply.group.entity.GroupMember;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> {
+  Optional<GroupMember> findByUserId(Long userId);
+
+  boolean existsByUserId(Long userId);
+
+  boolean existsByGroupIdAndUserId(Long groupId, Long userId);
+
+  List<GroupMember> findByGroupId(Long groupId);
+
+  void deleteByGroupId(Long groupId);
+}
