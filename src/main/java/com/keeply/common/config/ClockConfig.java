e3cebba6e63ca0c56add41d2ea2ac19f66426d99
huniversal <hunivers0523@naver.com>
2026-07-05 16:09:51 +0900

    fix: 애플리케이션 시간대 기준 명시

--- src/main/java/com/keeply/common/config/ClockConfig.java
+++ src/main/java/com/keeply/common/config/ClockConfig.java
@@ -1,14 +1,17 @@
 package com.keeply.common.config;
 
 import java.time.Clock;
+import java.time.ZoneId;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ClockConfig {
 
+  private static final ZoneId APPLICATION_ZONE_ID = ZoneId.of("Asia/Seoul");
+
   @Bean
   public Clock clock() {
-    return Clock.systemDefaultZone();
+    return Clock.system(APPLICATION_ZONE_ID);
   }
 }
